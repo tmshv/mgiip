eaeb7a99700a3e4bf640676b7f99aedfacf90946
Roman Timashev <roman@tmshv.ru>
2026-01-31 00:11:35 +0700

    fix popup flickering

--- app/components/map-popup/popup-with-style.tsx
+++ app/components/map-popup/popup-with-style.tsx
@@ -1,20 +1,16 @@
-import { useMemo, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { Popup } from "react-map-gl/mapbox";
 import type { PopupProps } from "react-map-gl/mapbox";
-import { useNextTick } from "~/hooks/use-next-tick";
 
-// this component will fix an issue from react-map-gl related to custom
-// max-width style for mapbox popup container
-// I dont know why but it is update it skip value of style on mount
-// and second render goes well
+// This component fixes an issue from react-map-gl where custom
+// max-width style is not applied on initial mount.
+// Using useLayoutEffect ensures style is applied before browser paint.
 const PopupWithStyle: React.FC<PopupProps> = ({ style, ...props }) => {
-    const [currentStyle, setStyle] = useState<React.CSSProperties | undefined>();
-    const callback = useMemo(() => {
-        return () => {
-            setStyle(style);
-        }
-    }, [style])
-    useNextTick(callback)
+    const [currentStyle, setStyle] = useState<React.CSSProperties | undefined>(style);
+
+    useLayoutEffect(() => {
+        setStyle(style);
+    }, [style]);
 
     return (
         <Popup
