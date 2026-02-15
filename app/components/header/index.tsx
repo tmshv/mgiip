4b868042e1915fa19828efdca2a66d37119d5149
Roman Timashev <roman@tmshv.ru>
2026-02-16 01:13:19 +0700

    shadow

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -12,7 +12,7 @@ export default function Header({ fields, datasetCount, onSearchSelect }: HeaderP
     return (
         <header className="header">
             <div className="header-brand">
-                <Logo width={166} height={10} fill="black" />
+                <Logo width={166} height={10} fill="white" />
                 <h1 className="header-title header-title--full">Статистика конкурса МГиИП 2018-2025</h1>
                 <h1 className="header-title header-title--short">МГиИП</h1>
             </div>
